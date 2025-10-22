e07d51245b25c3e2d3ade734a70f527d065406c1
namankaushik <kaushiknaman68@gmail.com>
2025-10-22 20:28:32 +0530

    SupplierDemo

---
+++ src/functionalInterface/inbuilt/SupplierDemo.java
@@ -0,0 +1,12 @@
+package functionalInterface.inbuilt;
+
+import java.util.function.Supplier;
+
+public class SupplierDemo {
+    static void main(String[] args) {
+        Supplier<Double> random = Math::random;
+        System.out.println(random.get());
+        System.out.println(random.get());
+        System.out.println(random.get());
+    }
+}
