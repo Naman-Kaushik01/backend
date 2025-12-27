c2ca496adc769c86741c246320901c00bf896622
namankaushik <kaushiknaman68@gmail.com>
2025-12-27 22:19:01 +0530

    Checking Values

--- src/day7OptionalInterface/NullPointerExceptionDemo.java
+++ src/day7OptionalInterface/NullPointerExceptionDemo.java
@@ -1,6 +1,7 @@
 package day7OptionalInterface;
 
 import java.sql.Struct;
+import java.util.Optional;
 
 public class NullPointerExceptionDemo {
     public  static void main(String[] args) {
@@ -11,6 +12,33 @@ public class NullPointerExceptionDemo {
         }else {
             System.out.println(name.length() );
         }
+
+        // Creating Optional
+
+        Optional<String> optionalString = Optional.of("Java");
+        System.out.println(optionalString);
+
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty);
+
+        //To insert null value we should use ofNullable instead of "of"
+        Optional<String> mayBe = Optional.ofNullable(null);
+        System.out.println(mayBe);
+
+        //Checking Values
+        System.out.println(mayBe.isPresent());
+        System.out.println(mayBe.isEmpty());
+
+        //get : if not sure that value is present or not don't use get it will give exception
+        System.out.println(optionalString.get());
+
+        /*How to retrieve value from optional safely ?
+         We have three methods :
+         1. orElse()
+         2.orElseGet()
+         3.orElseThrow()
+         */
+
 
 
 
